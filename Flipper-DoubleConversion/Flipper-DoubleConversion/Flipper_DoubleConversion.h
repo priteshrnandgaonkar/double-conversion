ae0c33a2835b2c017e827badcbb5c9beb7967e6a
Pritesh Nandgaonkar <prit91@fb.com>
2021-04-03 17:31:33 +0100

    Add Flipper-DoubleConversion xcodeproj

---
+++ Flipper-DoubleConversion/Flipper-DoubleConversion/Flipper_DoubleConversion.h
@@ -0,0 +1,18 @@
+//
+//  Flipper_DoubleConversion.h
+//  Flipper-DoubleConversion
+//
+//  Created by Pritesh Nandgaonkar on 4/3/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Flipper_DoubleConversion.
+FOUNDATION_EXPORT double Flipper_DoubleConversionVersionNumber;
+
+//! Project version string for Flipper_DoubleConversion.
+FOUNDATION_EXPORT const unsigned char Flipper_DoubleConversionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Flipper_DoubleConversion/PublicHeader.h>
+
+
